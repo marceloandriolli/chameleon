229351127f324425150649e629fd28d034fbe354
Marcelo Andriolli <marcelorsa@gmail.com>
2019-06-21 22:42:02 -0300

    disable brownseble api

--- server/settings.py
+++ server/settings.py
@@ -78,6 +78,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'server.wsgi.application'
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
+
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
